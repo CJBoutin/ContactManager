35e264052722c2c969461701127acb45c0d455c3
Connor Boutin <cjboutin@bellsouth.net>
2018-01-17 10:59:33 -0500

    Add Debug Console to Source control

---
+++ DebuggingConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DebuggingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "localhost:59056/";
+        }
+    }
+}
